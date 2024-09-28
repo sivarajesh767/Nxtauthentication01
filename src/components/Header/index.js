afe2a8dfe2fd47203ee32977cc648672fb5f5003
sivarajesh767 <sivarajesh767@gmail.com>
2024-09-28 17:33:45 +0000

    Error

---
+++ src/components/Header/index.js
@@ -0,0 +1,13 @@
+// Write your JS code here
+import {Link} from 'react-router-dom'
+
+const Header = () => {
+  <nav>
+    <ul>
+    <Link to="/">
+      <li>Home</li>
+    </Link>
+  </ul>
+  </nav>
+}
+export default Header
