1fac7b12e25324eed68a9c70176ec7498ec7896e
siva-r767 <167684678+sivarajesh767@users.noreply.github.com>
2024-09-29 17:51:24 +0530

    Update index.js

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,12 +1,12 @@
 import Header from '../Header'
 import './index.css'
 
-const Home = () => {
+const Home = () => (
   <>
     <Header />
     <div>
       <h1 className="head">Hello world</h1>
     </div>
   </>
-}
+)
 export default Home
